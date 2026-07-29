38fb74683a30c676d1b30b1fa199efb9470a29cf
Archit Boraste <architboraste2006@gmail.com>
2026-07-29 21:09:43 +0530

    init

---
+++ 01_blocking.js
@@ -0,0 +1,39 @@
+//aim-> to show that synchronous work hold js thread for itself, hence not even a sceduled timer 
+//can run
+
+const start = Date.now()
+
+const timeEllapsed = () => `+${String(Date.now() - start).padStart(4,'')}ms`
+//tells time elapsed since program started running
+
+const heartBeat = setInterval(()=>{
+    console.log(`   + heartbeat        ${timeEllapsed()}`);
+},100)
+//whenever node free, every 100 millisecond it will this
+
+function block(duration){
+    const finishedAt = Date.now() + duration
+    while(Date.now()<finishedAt){
+        //func not doing anything just blocking the thread
+    }
+}
+
+
+console.log(`1) start ${timeEllapsed()}`)
+
+//now we shall set timer which will execute 400 ms after program starts, and will call
+//block with 1000ms as the duration
+setTimeout(()=>{
+    console.log(`2) Blocking started at ${timeEllapsed()}`)
+    block(1000)
+    console.log(`3) Blocking ended at ${timeEllapsed()}`)
+},400)
+
+//stop the program after 2000 ms 
+//clearInterval stops a interval else setInterval will keep going on and node.js program wont 
+//stop, unless done by ctrl c
+setTimeout(()=>{
+    clearInterval(heartBeat)
+    console.log(`4) end ${timeEllapsed()}`)
+},2000)
+
