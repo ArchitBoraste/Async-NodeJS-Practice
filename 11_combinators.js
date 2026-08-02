ffc4089a1b9953dc9a5c092062eed7ec70d4293c
Archit Boraste <architboraste2006@gmail.com>
2026-08-03 00:13:09 +0530

    .

--- 11_combinators.js
+++ 11_combinators.js
@@ -11,3 +11,14 @@ const timeEllapsed = () => {
 
 //1. Promise.all ==> all or nothing, rejects on the first failure
 //we use this when we need all data and partial data is useless
+function demoAll(){
+    console.log('Fetch 3 cars concurrently')
+    const t0 = process.hrtime.bigint()
+    return Promise.all([findCar(101), findCar(202), findCar(303)])
+    .then((cars)=>{
+        const took = Number(process.hrtime.bigint() - t0)/1_000_000
+        console.log(`${cars.length} cars in ${took.toFixed(0)}ms`)//took is the time fetch all 3 cars, not the sum of time to fetch each car
+        cars.forEach((car)=>console.log(`${car.model.padEnd(18)} ${formatINR(car.pricePaise)}`))
+    })
+}
+
