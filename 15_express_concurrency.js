41d50b03adad73b16b4e94f5c680998f9c3a29a2
Archit Boraste <architboraste2006@gmail.com>
2026-08-08 15:33:52 +0530

    .

---
+++ 15_express_concurrency.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { findCar, findDealer, findOffers } from './09_fake_db.js';
+
+const app = express()
+
+const start = process.hrtime.bigint()
+const time = () => `${(Number(process.hrtime.bigint() - start) / 1_000_000).toFixed(0).padStart(5)}ms`
+
+app.get('/trace/:carId', async (req, res)=>{
+    const {carId} = req.params
+
+    //note on object destructuring:
+    
+    // What req.params actually looks like behind the scenes:
+    // {
+    //   carId: "101"
+    // }
+
+    // const carId = req.params; 
+    // console.log(carId); // Prints: { carId: '101' }
+
+    //const { carId } = req.params; 
+    //console.log(carId); --> Prints: '101'
+
+    // This is just a modern shortcut for writing the old way:
+    // const carId = req.params.carId;
+
+    console.log(`${time()} [${carId}] handler START`)
+    const car = await findCar(carId)
+    console.log(`${time()} Found car ${carId}`)
+
+    const dealer = await findDealer(car.dealerId)
+    console.log(`${time()} found delaer for ${carId}`)
+
+    const offers = await findOffers(dealer.id)
+    console.log(`${time()} found offers, ${carId} handler STOP`)
+
+    res.json({car, dealer, offers})
+})
